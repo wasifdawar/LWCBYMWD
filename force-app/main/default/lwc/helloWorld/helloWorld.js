7f85c4a8098c6c253e09e9bc2ee9e08378e17af6
Wasif Dawar <m.wasifdawar@gmail.com>
2024-08-26 15:09:58 +0500

    @track

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+
+    fullName = "Wasif Dawar"
+    course = "LWC";
+
+    handleChange(event)
+    {
+        this.course = event.target.value;
+    }
+
+    @track address = {
+
+        city:'Karachi',
+        postalCode: '74900',
+        country:'Pakistan'
+    }
+
+    handleTrack(event)
+    {
+      thia.address.city = event.target.value;
+    }
+}
